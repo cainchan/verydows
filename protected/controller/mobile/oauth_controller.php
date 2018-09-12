f8525ffd75a44c75f9ec0e618b6f314707eaea19
cainchan <kaychen.cn@gmail.com>
2018-09-13 01:09:42 +0800

    get weixin user info

--- protected/controller/mobile/oauth_controller.php
+++ protected/controller/mobile/oauth_controller.php
@@ -36,4 +36,44 @@ class oauth_controller extends general_controller
             jump(url('mobile/main', '404'));
         }
     }
+    public function action_index(){
+        $appid='wx10b5adf00912b287';
+        $redirect_uri = urlencode ( 'http://www.linkpod.cn/index.php?m=mobile&c=oauth&a=get_user_info' );
+        $url ="https://open.weixin.qq.com/connect/oauth2/authorize?appid=$appid&redirect_uri=$redirect_uri&response_type=code&scope=snsapi_base&state=1#wechat_redirect";
+        header("Location:".$url);
+    }
+    public function action_get_user_info(){
+        $appid = "wx10b5adf00912b287";  
+        $secret = "78dedbb3c365c360dca58343c2cdcb10";  
+        $code = $_GET["code"];
+         
+        //第一步:取全局access_token
+        $url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appid&secret=$secret";
+        $token = $this->getJson($url);
+         
+        //第二步:取得openid
+        $oauth2Url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=$appid&secret=$secret&code=$code&grant_type=authorization_code";
+        $oauth2 = $this->getJson($oauth2Url);
+          
+        //第三步:根据全局access_token和openid查询用户信息  
+        $access_token = $token["access_token"];  
+        $openid = $oauth2['openid'];  
+        $get_user_info_url = "https://api.weixin.qq.com/cgi-bin/user/info?access_token=$access_token&openid=$openid&lang=zh_CN";
+        $userinfo = $this->getJson($get_user_info_url);
+         
+        //打印用户信息
+          print_r($userinfo);
+         
+        
+    }
+    public function getJson($url){
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE); 
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE); 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $output = curl_exec($ch);
+        curl_close($ch);
+        return json_decode($output, true);
+    }
 }
