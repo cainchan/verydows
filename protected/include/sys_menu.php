435e4afab8fef13c3313b1d3b3e94a6805d09409
zhengkai <zhengkai.chen@ifchange.com>
2018-09-18 13:05:01 +0800

    delete some menu

--- protected/include/sys_menu.php
+++ protected/include/sys_menu.php
@@ -18,13 +18,11 @@ return array
         (
             '订单列表' => 'c=order&a=index',
             '发货列表' => 'c=order_shipping&a=index',
-            '订单日志' => 'c=order_log&a=index',
         ),
 
         '用户管理' => array
         (
             '用户列表' => 'c=user&a=index',
-            '用户组' => 'c=user_group&a=index',
             '账户日志' => 'c=user_account_log&a=index',
         ),
         
@@ -37,26 +35,12 @@ return array
             '访问统计' => 'c=stats&a=visitor',
             '友情链接' => 'c=friendlink&a=index',
         ),
-        
-        '广告管理' => array
-        (
-            '广告位列表' => 'c=adv_position&a=index',
-            '广告列表' => 'c=adv&a=index',
-        ),
-        
         '文章管理' => array
         (
             '资讯列表' => 'c=article&a=index',
             '资讯分类' => 'c=article_cate&a=index',
             '帮助列表' => 'c=help&a=index',
             '帮助分类' => 'c=help_cate&a=index',
-        ),
-        
-        '邮件管理' => array
-        (
-            '订阅列表' => 'c=email_subscription&a=index',
-            '邮件模板' => 'c=email_tpl&a=index',
-            '邮件队列' => 'c=email_queue&a=index',
         ),
     ),
     
@@ -75,11 +59,6 @@ return array
             '配送方式' => 'c=shipping_method&a=index',
             '支付方式' => 'c=payment_method&a=index',
             '物流承运商' => 'c=shipping_carrier&a=index',
-        ),
-        
-        '系统应用' => array
-        (
-            '授权登录' => 'c=oauth&a=index',
         ),
         
         '系统工具' => array
